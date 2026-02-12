2dc89b0bbf7497fa1ad11990d669792bd76188c7
zayn-tech-info <zayntech.info@gmail.com>
2026-02-12 23:25:37 +0100

    optimized the hero section images

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: ["beautiful-gorilla.outray.app"],
+    hmr: {
+      clientPort: 443,
+    },
+  },
 });
